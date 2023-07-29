2b34046d2d57da03a4c3dfa0d91694aec5612c42
houndhollis <houndhollis9797@gmail.com>
2023-07-29 14:13:50 +0900

    feat: 자동 로그인 버튼 추가

--- src/components/autoSignInCheckBox/autoSignInCheckBox.jsx
+++ src/components/autoSignInCheckBox/autoSignInCheckBox.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import styles from './AutoSigninCheckbox.module.scss';
 
-const autoSignInCheckBox = () => {
+import React from 'react';
+import CheckBox from '../checkBox/Checkbox';
+import Tooltip from '../tooltip/Tooltip';
+
+const AutoSignInCheckBox = ({ 
+  label = '자동 로그인',
+  checked, 
+  disabled,
+  orientation = 'top',
+  message = '개인 정보 보호를 위해 본인 기기에서만 이용해주세요.',
+  onChange,
+  ...resProps 
+}) => {
   return (
-    <div>autoSignInCheckBox</div>
+    <div className={styles.wrapper}>
+      <CheckBox
+        label={label}
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+        {...resProps}
+      />
+    {checked && (
+      <Tooltip
+        left={-5}
+        top={24}
+        orientation={orientation}
+        message={message}
+      />
+      )}
+    </div>
   )
 }
 
-export default autoSignInCheckBox
+export default AutoSignInCheckBox
