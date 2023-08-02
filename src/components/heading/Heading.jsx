973b880a9a22a3f801639d03c2660f6bb9869f0b
houndhollis <houndhollis9797@gmail.com>
2023-08-02 22:58:46 +0900

    feat: Heading 컴포넌트 추가

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from './Heading.module.scss';
 
-const Heading = () => {
+const Heading = ({
+  title,
+  subTitle,
+  center
+}) => {
   return (
-    <div>Heading</div>
+    <div className={styles.wrapper}>
+      <div className={center ? styles.center : ''}>
+        <div className={styles.title}>
+          {title}
+        </div>
+        <div className={styles.subTitle}>
+          {subTitle}
+        </div>
+      </div>
+    </div>
   )
 }
 
