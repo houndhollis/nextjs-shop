a8471bbbffd4cdef94808694bec9c1662805b6c9
houndhollis <houndhollis9797@gmail.com>
2023-08-07 21:20:12 +0900

    feat: rtk 설정 초기

---
+++ src/redux/provider.jsx
@@ -0,0 +1,15 @@
+'use client';
+import store from './store';
+import { Provider } from 'react-redux';
+
+import React from 'react';
+
+const Providers = ({children}) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  )
+}
+
+export default Providers
