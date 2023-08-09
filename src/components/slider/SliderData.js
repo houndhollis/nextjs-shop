9b6203ef1d5497d690a6e04103068d7d4e51d060
houndhollis <houndhollis9797@gmail.com>
2023-08-10 00:01:16 +0900

    style: 슬라이드 css 추가

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,19 @@
+const sliderData = [
+  {
+    image:'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    heading:'title 1',
+    desc:'description 1',  
+  },
+  {
+    image:'https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    heading:'title 2',
+    desc:'description 2',  
+  },
+  {
+    image:'https://images.unsplash.com/photo-1492571350019-22de08371fd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1353&q=80',
+    heading:'title 3',
+    desc:'description 3',  
+  },
+]
+
+export default sliderData
