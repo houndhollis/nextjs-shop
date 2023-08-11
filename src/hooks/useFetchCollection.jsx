ab1ebbff38518675282f360e7c8921ee8885462a
houndhollis <houndhollis9797@gmail.com>
2023-08-12 01:27:44 +0900

    feat: 파이어베이스 Collection 가져오는 함수

--- src/hooks/useFetchCollection.jsx
+++ src/hooks/useFetchCollection.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
+import React from 'react';
 
-const useFetchCollection = () => {
-  return (
-    <div>useFetchCollection</div>
-  )
+import { db } from '@/firebase/firebase';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+const useFetchCollection = (collectionName) => {
+
+  const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const getCollection = React.useCallback(() => {
+    setIsLoading(true);
+    try {
+      const docRef = collection(db, collectionName);
+      const q = query(docRef, orderBy('createdAt', 'desc'));
+
+      onSnapshot(q, (snapshot) => {
+        const allData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        setData(allData);
+        setIsLoading(false);
+      })
+
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  },[collectionName])
+
+  React.useEffect(() => {
+    getCollection();
+  },[getCollection])
+
+  return { data, isLoading };
 }
 
-export default useFetchCollection
+export default useFetchCollection 
