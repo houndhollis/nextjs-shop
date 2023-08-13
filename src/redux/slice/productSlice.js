21c04af47981019a3f408b86b2535e07f6a484bb
houndhollis <houndhollis9797@gmail.com>
2023-08-13 22:13:49 +0900

    feat: productSlice filter 적용 초기셋팅

--- src/redux/slice/productSlice.js
+++ src/redux/slice/productSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  products : []
+  products : [],
+  minPrice: null,
+  maxPrice: null,
 }
 
 const productSlice = createSlice({
@@ -10,12 +12,27 @@ const productSlice = createSlice({
   reducers: {
     STORE_PRODUCTS(state, action) {
       state.products = action.payload.products
+    },
+    GET_PRICE_RANGE: (state, action) => {
+      const { products } = action.payload;
+      const array = [];
+      products.map((product) => {
+        const price = product.price;
+        return array.push(price);
+      });
+      const max = Math.max(...array);
+      const min = Math.min(...array);
+
+      state.maxPrice = max;
+      state.minPrice = min;
     }
   }
 })
 
-export const { STORE_PRODUCTS } = productSlice.actions;
+export const { STORE_PRODUCTS, GET_PRICE_RANGE } = productSlice.actions;
 
 export const selectProducts = (state) => state.product.products;
+export const selectMinPrice = (state) => state.product.minPrice;
+export const selectMaxPrice = (state) => state.product.maxPrice;
 
 export default productSlice.reducer;
