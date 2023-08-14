807f83f66ea5a43f582efded5814fd7b49dc7f73
houndhollis <houndhollis9797@gmail.com>
2023-08-15 00:51:48 +0900

    feat: 리덕스 스토어 변경

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './slice/authSlice';
 import productReducer from './slice/productSlice';
+import filterReducre from './slice/filterSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  product: productReducer
+  product: productReducer,
+  filter: filterReducre
 })
 
 const store = configureStore({
