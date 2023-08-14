cee0d98809daaa5c3f99fdfda4168b1c1eb11ac0
houndhollis <houndhollis9797@gmail.com>
2023-08-15 00:51:31 +0900

    feat: 필터 슬라이스 추가

---
+++ src/redux/slice/filterSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  filteredProducts: []
+}
+
+const filterSlice = createSlice({
+  name:'filter',
+  initialState,
+  reducers: {
+    FILTER_BY_CATEGORY: (state, action) => {
+      const { products, category } = action.payload;
+      let tempProducts = [];
+      if (category === 'All') {
+        tempProducts = products
+      } else {
+        tempProducts = products.filter((product) => 
+           product.category === category
+        );
+      }
+      state.filteredProducts = tempProducts
+    },
+    FILTER_BY_BRAND: (state, action) => {
+      const { products, brand } = action.payload;
+      let tempProducts = [];
+      if (brand === 'All') {
+        tempProducts = products
+      } else{
+        tempProducts = products.filter((product) => 
+          product.brand = brand
+        )
+      }
+      state.filterd
+    },
+    FILTER_BY_PRICE: (state, action) => {
+      const { products, price } = action.payload;
+      let tempProducts = [];
+      tempProducts = products.filter((product) => product.price <= price);
+      state.filteredProducts = tempProducts;
+    }
+  }
+})
+
+export const { FILTER_BY_BRAND, FILTER_BY_CATEGORY, FILTER_BY_PRICE } = filterSlice.actions;
+
+export const selectFilterProducts = (state) => state.filter.filteredProducts;
+
+export default filterSlice.reducer
