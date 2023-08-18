dbf12bde8d6b47b30f293f59bdc2ddf0284e13b0
houndhollis <houndhollis9797@gmail.com>
2023-08-18 23:37:40 +0900

    feat: Product 컴포넌트 추가

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -3,20 +3,46 @@ import styles from './Product.module.scss';
 
 import React from 'react';
 import useFetchCollection from '@/hooks/useFetchCollection';
+import { useDispatch, useSelector } from 'react-redux';
+import { GET_PRICE_RANGE, selectMaxPrice, selectProducts, STORE_PRODUCTS } from '@/redux/slice/productSlice';
+import Loader from '../loader/Loader';
+import ProductList from './productList/ProductList';
+import ProductFilter from './productFilter/ProductFilter';
 
 const Product = () => {
 
-  // const {data, isLoading} = useFetchCollection('products');
+  const {data, isLoading} = useFetchCollection('products');
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data
+      })
+    )
+    dispatch(
+      GET_PRICE_RANGE({
+        products: data
+      })
+    )
+  },[data, dispatch])
+  
+  const products = useSelector(selectProducts);
 
   return (
     <section className={styles.product}>
       <aside
         className={styles.filter}
       >
-        aside
+        {isLoading ? null : <ProductFilter/>}
       </aside>
       <div className={styles.content}>
-        content
+        {isLoading ? (
+          <Loader basic/>
+        ) : (
+          <ProductList/>
+        )}
       </div>
     </section>
   )
