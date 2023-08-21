776933368b03fee30b1f23b91d388494d739ad77
houndhollis <houndhollis9797@gmail.com>
2023-08-21 22:47:36 +0900

    feat: 상품상세 리뷰 섹션 추가

---
+++ src/components/product/productReviewItem/ProductReviewItem.jsx
@@ -0,0 +1,25 @@
+import { formatTime } from '@/utils/dayjs';
+import React from 'react';
+import { Rating } from 'react-simple-star-rating';
+import styles from './ProductReviewItem.module.scss';
+
+const ProductReviewItem = ({rate, review, reviewDate, userName}) => {
+  return (
+    <div className={styles.review}>
+      <p className={styles.writer}>
+        {userName} <span>님의 상품평</span>
+      </p>
+      <Rating
+        initialValue={rate}
+        readonly
+        size={25}
+      />
+      <p className={styles.content}>{review}</p>
+      <p className={styles.date}>
+        {formatTime(reviewDate)}
+      </p>
+    </div>
+  )
+}
+
+export default ProductReviewItem
