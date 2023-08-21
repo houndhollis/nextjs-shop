c19fead8b8779447d470237ef5f65e45819d8f9d
houndhollis <houndhollis9797@gmail.com>
2023-08-21 22:34:05 +0900

    feat: cartSlice 추가

---
+++ src/redux/slice/cartSlice.js
@@ -0,0 +1,73 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const initialState = {
+  cartItems: typeof window !== "undefined" ? 
+    localStorage.getItem('cartItems') 
+      ? JSON.parse(localStorage.getItem('cartItems')) : [] : [],
+  cartTotalQuantity:0,
+  cartTotalAmount:0,
+  previousURL:'',
+}
+
+const cartSlice = createSlice({
+  name:'cart',
+  initialState,
+  reducers: ({
+    ADD_TO_CART: (state,action) => {
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id,
+      )
+
+      const increaseCount = action.payload.quantity ? action.payload.quantity : 1
+      
+      if (productIndex >= 0) {
+        state.cartItems[productIndex].cartQuantity += increaseCount,
+        toast.success(`${action.payload.name} 상품이 하나 추가되었습니다.`);
+      } else {
+        const tempProduct = {...action.payload, cartQuantity: increaseCount}
+        state.cartItems.push(tempProduct)
+        toast.success(`${action.payload.name} 상품이 하나 추가되었습니다.`);
+      }
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+    },
+    CALCULATE_TOTAL_QUANTITY: (state, action) => {
+      const array = [];
+      state.cartItems.map((item) => {
+        const { cartQuantity } = item;
+        const quantity = cartQuantity;
+
+        return array.push(quantity)
+      })
+
+      const totalQuantity = array.reduce((a,b) => {
+        return a + b
+      },0)
+      state.cartTotalQuantity = totalQuantity
+    },
+    CALCULATE_SUBTOTAL: (state, action) => {
+      const array = [];
+      state.cartItems.map((item) => {
+        const { price, cartQuantity } = item;
+        const cartItemAmount = price * cartQuantity;
+        return array.push(cartItemAmount);
+      })
+      const totalAmount = array.reduce((a,b) => {
+        return a + b
+      },0)
+      state.cartTotalAmount = totalAmount;
+    },
+    SAVE_URL: (state, action) => {
+      state.previousURL = action.payload;
+    }
+  })
+})
+
+export const { ADD_TO_CART, CALCULATE_TOTAL_QUANTITY, CALCULATE_SUBTOTAL, SAVE_URL } = cartSlice.actions;
+
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartTotalQuantity = (state) => state.cart.cartTotalQuantity;
+export const selectCartTotalAmount = (state) => state.cart.cartTotalAmount;
+
+export default cartSlice.reducer
+
